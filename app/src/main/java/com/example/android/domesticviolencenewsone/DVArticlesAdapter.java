f0850b0d1813db748352caade1455677347af446
Theda Rogers <crayonwriter@gmail.com>
2018-08-12 22:41:36 -0400

    feat: create textviews in DVArticlesAdapter

--- app/src/main/java/com/example/android/domesticviolencenewsone/DVArticlesAdapter.java
+++ app/src/main/java/com/example/android/domesticviolencenewsone/DVArticlesAdapter.java
@@ -10,10 +10,12 @@ import android.widget.TextView;
 
 import com.example.android.domesticviolencenewsone.R;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
-public class DVArticlesAdapter extends ArrayAdapter<DVArticles>{
-   //Constructor
+public class DVArticlesAdapter extends ArrayAdapter<DVArticles> {
+    //Constructor
     public DVArticlesAdapter(Context context, List<DVArticles> dvArticles) {
         super(context, 0, dvArticles);
     }
@@ -32,77 +34,29 @@ public class DVArticlesAdapter extends ArrayAdapter<DVArticles>{
         // Find the article at the given position in the list of articles
         DVArticles currentDVArticles = getItem(position);
 
-        // Find the TextView with view ID magnitude
-        TextView magnitudeView = (TextView) listItemView.findViewById(R.id.Magnitude);
-        // Format the magnitude to show 1 decimal place
-        String formattedMagnitude = formatMagnitude(currentEarthquake.getMagnitude());
-        // Display the magnitude of the current earthquake in that TextView
-        magnitudeView.setText(formattedMagnitude);
+        // Find the TextView with view ID section
+        TextView sectionView = (TextView) listItemView.findViewById(R.id.section);
+        // Display the section of the current article in that TextView
+        sectionView.setText(currentDVArticles.getSection());
 
-        // Set the proper background color on the magnitude circle.
-        // Fetch the background from the TextView, which is a GradientDrawable.
-        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
-        // Get the appropriate background color based on the current earthquake magnitude
-        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
-        // Set the color on the magnitude circle
-        magnitudeCircle.setColor(magnitudeColor);
+        // Find the TextView with view ID url
+        TextView urlView = (TextView) listItemView.findViewById(R.id.url);
+        urlView.setText(currentDVArticles.getUrl());
 
-        // Get the original location string from the Earthquake object,
-        // which can be in the format of "5km N of Cairo, Egypt" or "Pacific-Antarctic Ridge".
-        String originalLocation = currentEarthquake.getLocation();
+        TextView headlineView = listItemView.findViewById(R.id.headline);
+        headlineView.setText(currentDVArticles.getHeadline());
 
-        // If the original location string (i.e. "5km N of Cairo, Egypt") contains
-        // a primary location (Cairo, Egypt) and a location offset (5km N of that city)
-        // then store the primary location separately from the location offset in 2 Strings,
-        // so they can be displayed in 2 TextViews.
-        String primaryLocation;
-        String locationOffset;
+        TextView bylineView = listItemView.findViewById(R.id.byline);
+        bylineView.setText(currentDVArticles.getByline());
 
-        // Check whether the originalLocation string contains the " of " text
-        if (originalLocation.contains(LOCATION_SEPARATOR)) {
-            // Split the string into different parts (as an array of Strings)
-            // based on the " of " text. We expect an array of 2 Strings, where
-            // the first String will be "5km N" and the second String will be "Cairo, Egypt".
-            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
-            // Location offset should be "5km N " + " of " --> "5km N of"
-            locationOffset = parts[0] + LOCATION_SEPARATOR;
-            // Primary location should be "Cairo, Egypt"
-            primaryLocation = parts[1];
-        } else {
-            // Otherwise, there is no " of " text in the originalLocation string.
-            // Hence, set the default location offset to say "Near the".
-            locationOffset = getContext().getString(R.string.near_the);
-            // The primary location will be the full location string "Pacific-Antarctic Ridge".
-            primaryLocation = originalLocation;
-        }
+        TextView dateView = listItemView.findViewById(R.id.date);
+        dateView.setText(currentDVArticles.getDate());
 
-        // Find the TextView with view ID location
-        TextView primaryLocationView = (TextView) listItemView.findViewById(R.id.primary_location);
-        // Display the location of the current earthquake in that TextView
-        primaryLocationView.setText(primaryLocation);
-
-        // Find the TextView with view ID location offset
-        TextView locationOffsetView = (TextView) listItemView.findViewById(R.id.location_offset);
-        // Display the location offset of the current earthquake in that TextView
-        locationOffsetView.setText(locationOffset);
-
-        // Create a new Date object from the time in milliseconds of the earthquake
-        Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
-
-        // Find the TextView with view ID date
-        TextView dateView = (TextView) listItemView.findViewById(R.id.Date);
-        // Format the date string (i.e. "Mar 3, 1984")
-        String formattedDate = formatDate(dateObject);
-        // Display the date of the current earthquake in that TextView
-        dateView.setText(formattedDate);
-
-        // Find the TextView with view ID time
-        TextView timeView = (TextView) listItemView.findViewById(R.id.Time);
-        // Format the time string (i.e. "4:30PM")
-        String formattedTime = formatTime(dateObject);
-        // Display the time of the current earthquake in that TextView
-        timeView.setText(formattedTime);
+        TextView bodyView = listItemView.findViewById(R.id.body);
+        bodyView.setText(currentDVArticles.getBody());
 
         // Return the list item view that is now showing the appropriate data
         return listItemView;
     }
+
+}
