bcdfa505e2d688952dff27e0a8ae950ac6c4aa8a
Theda Rogers <crayonwriter@gmail.com>
2018-08-26 00:11:41 -0400

    fix: update code to reflect multi preference items

--- app/src/main/java/com/example/android/domesticviolencenewsone/SettingsActivity.java
+++ app/src/main/java/com/example/android/domesticviolencenewsone/SettingsActivity.java
@@ -2,14 +2,20 @@ package com.example.android.domesticviolencenewsone;
 
 import android.content.SharedPreferences;
 import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +35,20 @@ public class SettingsActivity extends AppCompatActivity {
 
             Preference sectionId = findPreference(getString(R.string.settings_section_key));
             bindPreferenceSummaryToValue(sectionId);
-
-
         }
+
+        //ANTI-PLAGIARISM ALERT!! I got help with this code from a Udacity mentor. I was in over my head with the MultiSelectListPreference
 
         private void bindPreferenceSummaryToValue(Preference preference) {
             preference.setOnPreferenceChangeListener(this);
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
-            String preferenceString = preferences.getString(preference.getKey(), "");
-            onPreferenceChange(preference, preferenceString);
+            if (preference instanceof MultiSelectListPreference) {
+                Set<String> values = preferences.getStringSet(preference.getKey(), null);
+                onPreferenceChange(preference, values);
+            } else {
+                String preferenceString = preferences.getString(preference.getKey(), "");
+                onPreferenceChange(preference, preferenceString);
+            }
         }
 
         @Override
@@ -50,10 +61,28 @@ public class SettingsActivity extends AppCompatActivity {
                     CharSequence[] labels = listPreference.getEntries();
                     preference.setSummary(labels[prefIndex]);
                 }
+            } else if (preference instanceof MultiSelectListPreference) {
+                List<String> newValues = new ArrayList<>((HashSet<String>) value);
+                preference.setSummary(TextUtils.join(", ", getSummaryListFromValueList(newValues)));
             } else {
                 preference.setSummary(stringValue);
             }
             return true;
+        }
+
+        private List<String> getSummaryListFromValueList(List<String> valueList) {
+            String[] allSummaries = getResources().getStringArray(R.array.settings_section_labels);
+            String[] allValues = getResources().getStringArray(R.array.settings_section_values);
+
+            List<String> summaryList = new ArrayList<>();
+            for (int i = 0; i < allValues.length; i++) {
+                for (String value : valueList) {
+                    if (allValues[i].equals(value)) {
+                        summaryList.add(allSummaries[i]);
+                    }
+                }
+            }
+            return summaryList;
         }
     }
 }
